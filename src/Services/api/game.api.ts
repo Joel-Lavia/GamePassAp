191848641f1c5d1990dbc6b22ee10017a2b44378
Joel-Lavia <joeldisidi8@gmail.com>
2026-06-25 21:36:01 +0100

    config axios

--- src/Services/api/game.api.ts
+++ src/Services/api/game.api.ts
@@ -1,6 +1,6 @@
 import { api } from "./axios";
 
-export async function listGames() {
+export async function listGamesRequest() {
   const games = await api.get("");
   return games.data;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
 import { Text, View } from "react-native";
-import { styles } from "./styles/index.style";
+import { useGetListgames } from "../hooks/useGetListGame";
+import { styles } from "../styles/index.style";
 
 export default function Index() {
+  const { isPending, isError, data, error } = useGetListgames();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Bonjours le monde.</Text>
